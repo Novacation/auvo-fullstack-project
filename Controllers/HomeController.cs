151bff018477680ef947ecc24d38bc7c218d3b85
NoVacation <andersoncelestino.n@gmail.com>
2024-10-02 08:24:29 -0300

    Auth updates

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboClimaPlatform.Controllers;
+
+[Route("home")]
+public class HomeController : Controller
+{
+    [Authorize]
+    public IActionResult HomeView()
+    {
+        var name = User.Claims.FirstOrDefault(item => item.Type.Equals(ClaimTypes.Name));
+
+        if (name is not null) return View("Home");
+        TempData["Error"] = "Você não está logado!";
+        return RedirectToAction("LoginView", "Auth");
+    }
+}
---
+++ Infra/Middlewares/JwtCookieMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace GloboClimaPlatform.Infra.Middlewares;
+
+public class JwtCookieMiddleware(RequestDelegate _next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Cookies.ContainsKey("JwtToken"))
+        {
+            var token = context.Request.Cookies["JwtToken"];
+            if (!string.IsNullOrEmpty(token))
+            {
+                context.Request.Headers.Append("Authorization", "Bearer " + token);
+            }
+        }
+
+        await _next(context);
+    }
+}
